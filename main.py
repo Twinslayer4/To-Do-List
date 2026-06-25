2dc9f59d4a4a9002c3054c1cdf814a694913eaaa
Steven Mejia <boyishtomb519@gmail.com>
2026-06-25 10:37:24 -0400

    Made Basic Code for To Do List

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+def main():
+    commandNumber = 0
+    taskLists = [];
+    while commandNumber != 4:
+        commandNumber = int(input("Enter 1 to input new task.\nEnter 2 to print out tasks.\nEnter 3 to delete a task.\nEnter 4 to end program.\n"))
+        if commandNumber == 1: # Input new tasks
+            taskLists.append(input("Enter new task: "));
+        elif commandNumber == 2:
+            for i in range(len(taskLists)):
+                print(f"{i+1}: {taskLists[i]}")
+        elif commandNumber == 3: #Delete a task
+            taskToDelete = int(input("Enter the number of the task to be deleted.")) -1
+            taskLists.pop(taskToDelete)
+        elif commandNumber == 4: #Quit program
+            return;
+        else: 
+            print("Not a valid command")
+
+main()
